8a783f3f0ef2fffbb30ec84e3fbe1508ab532d0b
Vlad Anokhin <69010351+vladanokhin@users.noreply.github.com>
2022-08-08 13:52:50 +0300

    Start telegram client if founded a new post

--- celery_parsing/tasks.py
+++ celery_parsing/tasks.py
@@ -6,10 +6,11 @@ from .telethon_client import TelegramBot
 @app.task(retries=3, default_retry_delay=1)
 def parse_olx():
     olx = OlxParser()
-    client = TelegramBot()
     houses = olx.search_new_houses()
     olx._browser.quit()
+
     if len(houses) > 0:
+        client = TelegramBot()
         msg = olx.prepare_message(houses)
         client.notification_users(msg)
 
